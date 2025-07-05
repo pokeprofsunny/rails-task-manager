7760e7c18c168d2794a3f70de1f18330b4531b44
Sunny Jayswal <sunnyjuw@gmail.com>
2025-07-05 16:30:27 +0900

    edit tasks implemented

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -20,6 +20,19 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def task_params
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,11 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   get 'tasks', to: 'tasks#index'
   get 'tasks/new', to: 'tasks#new', as: :new_task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
   get 'tasks/:id', to: 'tasks#show', as: :task
   post 'tasks', to: 'tasks#create'
+
 
   # Defines the root path route ("/")
   # root "posts#index"
